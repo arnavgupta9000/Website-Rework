2a4621381fadd2ecff3f1f5301cb085e79771997
Arnav Gupta <arnavgupta9000@gmail.com>
2025-02-18 16:24:38 -0700

    testing nav bar

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    const navLinks = document.querySelector("#main-nav ul");
+
+    hamburgerMenu.addEventListener("click", function () {
+        navLinks.classList.toggle("show");  // Toggle the "show" class to display the menu
+    });
+});
