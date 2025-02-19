f72e86356030818f36b1011e8377c657eb1aaa9b
Arnav Gupta <arnavgupta9000@gmail.com>
2025-02-18 18:07:16 -0700

    made the js for clearing

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 document.addEventListener("DOMContentLoaded", () =>  {
+    
     let hamburgerMenu = document.getElementById("hamburger-menu");
     let navLinks = document.querySelector("#main-nav ul");
     hamburgerMenu.addEventListener("click", () => {
         navLinks.classList.toggle("show");  
     });
+
+    function clear(id) {
+        document.querySelectorAll("#main > div").forEach(div => { // only select direct div children of main
+                div.style.display = "none";
+            
+        });
+        let selectedDiv = document.getElementById(id);
+
+        if (selectedDiv) { // if it exists
+            selectedDiv.style.display = "block";
+            selectedDiv.querySelectorAll("div").forEach(child => {
+                child.style.display = "block";
+            })
+        }
+
+        
+    }
+
+    document.getElementById("about-link").addEventListener("click", () => {
+        clear("about");
+    });
+
 });
